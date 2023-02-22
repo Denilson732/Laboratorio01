334933ff5d9f41eb675de8a1ea0bd4227e0edad6
ALUMNO <ALUMNO@CC1PC23-24>
2023-02-22 15:06:28 -0600

    Agregar archivos de proyecto.

---
+++ Laboratorio01/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona[] persona = new Persona[0];
+            persona[0] = new Persona[];
+
+
+            string[] SanSalvador = { "Apopa", "Aguilares", "Cuscatancingo", "El Paisnal", "Guazapa", "Mejicanos" };
+            string[] Chalatenango = { "San Antonio los Ranchos", "San Antonio de la Cruz", "Arcatao" };
+            string[] Morazan = { "Cacaopera", "Jocoro", "San Carlos", "San Fernando", "San Isidro", "Perquin", "Torola" };
+            string[] SantaAna = { "El Congo", "Masauat", "Chalcuapa", "Coatepeque", "Santa Ana" };
+            string[] SanMiguel = { "Carolina", "Chapeltique", "Chinameca", "Chirilagua", "Ciudad Barrios", "Comacarán", "El Tránsito", "Lolotique" };
+        }
+    }
+}
+
+
+public class Persona
+{
+    public string nombre { get; set; }
+    public int edad { get; set; }
+    public string Departamento { get; set; }
+    public string Municipio { get; set; }
+}
+
+
+
+Console.Write("Ingrese el nombre de la persona: ");
+string nombre = Console.ReadLine();
+
+
+
+
+
